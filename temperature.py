276605c67d59a8560133d10646a17f66e5f0e9cb
mattieuxfournier <mattieuxfournier@gmail.com>
2025-04-07 13:48:44 -0400

    final

--- temperature.py
+++ temperature.py
@@ -1,20 +1,35 @@
-import json
-import numpy as np
-import math
-
 def toCelcius(F):
     C = round(float((F-32)*(5/9)),2)
     return C
 data = open('data.txt', 'r')
-temp_dict = data.read().split()[24:]
-lst = list(map(float, temp_dict))
-temp2 ={}
-for line in lst:
-    lst[0] = int(lst[0])
-    year = lst[0]
-    lst.pop(0)
-    lst = list(map(toCelcius, lst))
-    temp2 = temp2.update({year:lst})
-    print(temp2)
-def avgTempYear():
-    1
+temp_dict = {}
+data = data.read().split()
+start = []
+for i in range(len(data)):
+    if data[i] == '1964':
+        start.append(i)
+start.remove(start[0])
+start = start[-1]
+data = list(map(float,data[start:]))
+years = []
+for i in data:
+    if len(str(i)) >= 5:
+        data[data.index(i)] = int(i)
+        years.append(data.index(i))
+temps = []
+for i in range(len(years)-1):
+    temps.append([years[i]+1, years[i+1]-1])
+for i in range(len(data)):
+    if type(data[i]) == float:
+        data[i] = toCelcius(data[i])
+for i in range(len(years)-1):
+    year = data[years[i]]
+    start = years[i] +1
+    end = years[i+1]
+    year_temps = data[start:end]
+    temp_dict[year] = year_temps
+start = temps[-1][0]
+end = temps[-1][1]
+year = years[-1]
+temp_dict[data[year]] = data[start:end]
+print(temp_dict)
